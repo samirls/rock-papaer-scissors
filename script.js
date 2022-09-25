517413a8017ef0a72ca6ae18026387abf6d15e70
samirls <samirlaguardiaii@gmail.com>
2022-09-24 22:31:25 -0300

    added interface

--- script.js
+++ script.js
@@ -2,103 +2,110 @@ console.log("Game Starts!");
 
 let playerScore = 0;
 let computerScore = 0;
+const winLose = 5;
 
 function getComputerChoice() {
   let choiceNum = Math.floor(Math.random() * 3);
   return choiceNum; 
 }
 
-function playerChoice() {
-  let playerCommand = prompt("Choose and write below: Rock, Paper, or Scissors?").toLowerCase();
-  if (playerCommand === "rock") {
-      return 0;
-  }
-  if (playerCommand === "paper") {
-      return 1;
-  }
-  if (playerCommand === "scissors") {
-      return 2;
-  }
-  else {
-    alert("Write just rock, paper or scissors. Whatch out for misspelling");
-  }
-  return playerChoice();
+document.getElementById("rockButton").addEventListener("click", playerSelectRock);
+document.getElementById("paperButton").addEventListener("click", playerSelectPaper);
+document.getElementById("scissorsButton").addEventListener("click", playerSelectScissors);
+
+
+function playerSelectRock() {
+  return playRound(0, getComputerChoice());
 }
+
+function playerSelectPaper() {
+  return playRound(1, getComputerChoice());
+}
+
+function playerSelectScissors() {
+  return playRound(2, getComputerChoice());
+}
+
+function win() {
+    if (playerScore == 5) {
+    document.getElementById("winLose").textContent = "Player Wins!!!";
+    document.getElementById("winLose2").textContent = "Press F5 to restart";
+    document.getElementById("buttons").textContent = "";
+    document.getElementById("removeL").textContent = "";
+    document.getElementById("removeL2").textContent = "";
+  }
+}
+
+function lose() {
+  if (computerScore == 5) {
+  document.getElementById("winLose").textContent = "Player Loses!";
+  document.getElementById("winLose2").textContent = "Press F5 to restart";
+  document.getElementById("buttons").textContent = "";
+  document.getElementById("removeL").textContent = "";
+  document.getElementById("removeL2").textContent = "";
+}
+}
+
 
 function playRound(playerSelection, computerSelection) {
   
   if (playerSelection == 0 && computerSelection == 0) {
-    playerScore++;
-    computerScore++;
-    return "Computer chooses Rock. That's a tie! Both players get 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Rock. That's a tie! No points given.";
 
   };
   if (playerSelection == 0 && computerSelection == 1) {
     computerScore++;
-    return "Computer chooses Paper. Computer gets 1 point."; 
+    document.getElementById("scores").textContent = "Computer chooses Paper. Computer gets 1 point.";
+    document.getElementById("scoreMarkerC").textContent = computerScore;
+    lose();
+
 
   };
   if (playerSelection == 0 && computerSelection == 2) {
     playerScore++;
-    return "Computer chooses Scissors. Player gets 1 pont.";
+    document.getElementById("scores").textContent = "Computer chooses Scissors. Player gets 1 pont.";
+    document.getElementById("scoreMarkerP").textContent = playerScore;
+    win();
 
   };
   if (playerSelection == 1 && computerSelection == 0) {
     playerScore++;
-    return "Computer chooses Rock. Player gets 1 pont.";
+    document.getElementById("scores").textContent = "Computer chooses Rock. Player gets 1 pont.";
+    document.getElementById("scoreMarkerP").textContent = playerScore;
+    win();
 
   };
   if (playerSelection == 1 && computerSelection == 1) {
-    playerScore++;
-    computerScore++;
-    return "Computer chooses Paper. That's a tie! Both players get 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Paper. That's a tie! No points given.";
 
   };
   if (playerSelection == 1 && computerSelection == 2) {
     computerScore++;
-    return "Computer chooses Scissors. Computer gets 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Scissors. Computer gets 1 point.";
+    document.getElementById("scoreMarkerC").textContent = computerScore;
+    lose();
+
 
   };
   if (playerSelection == 2 && computerSelection == 0) {
     computerScore++;
-    return "Computer chooses Rock. Computer gets 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Rock. Computer gets 1 point.";
+    document.getElementById("scoreMarkerC").textContent = computerScore;
+    lose();
+
 
   };
   if (playerSelection == 2 && computerSelection == 1) {
     playerScore++;
-    return "Computer chooses Paper. Player gets 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Paper. Player gets 1 point.";
+    document.getElementById("scoreMarkerP").textContent = playerScore;
+    win();
 
   };
   if (playerSelection == 2 && computerSelection == 2) {
-    playerScore++;
-    computerScore++;
-    return "Computer chooses Scissors. That's a tie! Both players get 1 point.";
+    document.getElementById("scores").textContent = "Computer chooses Scissors. That's a tie! No points given.";
   };
 }
 
-function game() {
-  for (i = 0; i < 5; i++) {
-    const playerSelection = playerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (playerScore > computerScore) {
-    console.log(`Player wins! You scored ${playerScore} points!`);
-    console.log(`Computer scored ${computerScore} points.`);
-  }
-  if (playerScore < computerScore) {
-    console.log(`You lose! You scored ${playerScore} points!`);
-    console.log(`Computer scored ${computerScore} points.`);
-  }
-  if (playerScore == computerScore) {
-    console.log(`That's a tie! You and the computer scored ${computerScore}.`);
-  }
-
-}
-
-game();
-
 
 console.log("Game Over!");
-
-//11:53
